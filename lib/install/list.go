592826745ac45f30d0062f72052fdcc175c0a130
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2023-11-11 16:47:01 +0000

    modify install options

--- lib/install/list.go
+++ lib/install/list.go
@@ -9,10 +9,11 @@ func list() *installList {
 		{"go", "Golang compiler", nil, true, nil},
 		{"flatpak", "Package manager", &pkg{"flatpak", nil, "flatpak", ""}, false, ConfigureFlatpak},
 		{"codium", "Code editor", &pkg{"", nil, "", "com.vscodium.codium"}, false, nil},
-		{"starship", "Terminal host", nil, true, nil},
+		{"starship", "Shell prompt", nil, true, nil},
 		{"aup", "Package manager", nil, true, nil},
 		{"scaffold", "Project templating tool", &pkg{"", &aupPkg{"lspaccatrosi16/scaffold", "scaffold-linux", "scaffold"}, "", ""}, false, nil},
 		{"releasetool", "Automatic github releaser", &pkg{"", &aupPkg{"lspaccatrosi16/releasetool", "releasetool-linux", "release"}, "", ""}, false, nil},
+		{"bupload", "Quick uploader to / from cloud buckets", &pkg{"", &aupPkg{"lspaccatrosi16/bupload", "bupload-linux", "bupload"}, "", ""}, false, nil},
 	}
 	return &list
 }
